cdab9fd4f0543ec7f83107fb8ed2afd3adb574ca
Dymond <dymond.hoffman03@gmail.com>
2022-11-13 18:03:01 -0800

    added plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { assert } = require('console');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,10 +19,41 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generate HTML and injects bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      // Injects custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Create amainfest.json file
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description:'Just another text editor!',
+        fingerprints: false,
+        inject: true,
+        background_color: '#f6c8fd',
+        theme_color: '#835d8f',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          },
+        ],
+      })
     ],
 
     module: {
+      //CSS loaders
       rules: [
         {
           test: /\.css$/i,
@@ -30,6 +62,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // Use babel-loader to use ES6
           use: {
             loader: 'babel-loader',
             options: {
